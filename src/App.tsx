e0019c39ccaefb358159f649180a400ac7fdcfb8
Your Name <you@example.com>
2023-01-09 00:33:58 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.scss";
-import { useState } from "react";
-
+import { useStore } from "./store";
+import { InfoBox } from "./components/InfoBox";
+import { useRef, useEffect } from "react";
 function App() {
-  const [message, setMessage] = useState("test");
+  const techBookSearchRef = useRef<HTMLInputElement>(null);
+  const store = useStore((state) => state);
+  useEffect(() => {
+    if (techBookSearchRef.current !== null) {
+      techBookSearchRef.current.focus();
+    }
+  }, [store.techBooks]);
+
   return (
     <div className="App">
       <h1>Demo zustand</h1>
@@ -12,14 +20,75 @@ function App() {
             <label>Message:</label>
             <input
               type="text"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
+              onChange={(e) => store.setMessage(e.target.value)}
             />
+            Message: {store.message}
+          </div>
+          <div className="data">
+            <label>Colors:</label>
+            <div>
+              <button onClick={() => store.addColor("blue")}>blue</button>
+              <button onClick={() => store.addColor("red")}>red</button>
+              <button onClick={() => store.addColor("yellow")}>yellow</button>
+            </div>
+            <button onClick={() => store.deleteColor()}>delete color</button>
+            <button onClick={() => store.setColors([])}>
+              delete all colors
+            </button>
+          </div>
+          {/*MULTIPLE VALUES */}
+          <div className="data">
+            <label>Change multiple values:</label>
+            <div>
+              <button onClick={() => store.deleteVowelsAndColorRed()}>
+                delete vowels from message and color red from colors
+              </button>
+            </div>
+          </div>
+          {/* Object */}
+          <div className="data">
+            <label>Object:</label>
+            <div>
+              <button onClick={() => store.toggleCurrentUserStatusOnline()}>
+                toggle online status
+              </button>
+              <button onClick={() => store.toggleCurrentUserStatusEmail()}>
+                toggle email status
+              </button>
+            </div>
+          </div>
+          <div className="data">
+            {store.techBooks.length === 0 && (
+              <div className="data">
+                <label>Load data</label>
+                <div>
+                  <button
+                    disabled={store.techBooksAreLoading}
+                    onClick={() => store.loadTechBooks()}
+                  >
+                    load tech books
+                  </button>
+                </div>
+              </div>
+            )}
+
+            {store.techBooks.length > 0 && (
+              <div className="data">
+                <label>Search tech books:</label>
+                <input
+                  type="text"
+                  ref={techBookSearchRef}
+                  value={store.techBookSearch}
+                  onChange={(e) => store.setTechBookSearch(e.target.value)}
+                />
+              </div>
+            )}
           </div>
         </section>
+        <section className="dataArea">
+          <InfoBox />
+        </section>
       </main>
-      <p>Welcome to this site.</p>
-      <p>Message: {message}</p>
     </div>
   );
 }
